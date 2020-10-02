e53e00abd485199fa991774d0d9e498fa25f1dc4
Lukasz-Ziemnicki <lukasz.ziemnicki@o2.pl>
2020-10-02 19:01:19 +0200

    controller with actions defined & routes set up

---
+++ app/controllers/api/v1/motorcycles_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module V1
+    class MotorcyclesController < ::ApplicationController
+      def create; end
+
+      def index; end
+
+      def update; end
+
+      def destroy ;end
+    end
+  end
+end
